8dcdf6c6ffb4420568a84b2a61b159ff67bf8e79
vikash <vikashsinghs5530@gmail.comm>
2025-06-26 21:00:25 +0530

    setup tailwind css shadcn button

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-red-500">
+      Hello world!
+    </h1>
+    <Button>Click me</Button>
+    </div>
+  )
+}
+
+export default App
